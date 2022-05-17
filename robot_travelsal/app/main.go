faa63596ee6a786e3125a696a83835c073b4ba9a
Keshav Bhardwaj <kkeshav025@gmail.com>
2022-05-17 11:53:50 +0530

    updated

--- robot_travelsal/app/main.go
+++ robot_travelsal/app/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	r := &robot.Robot{State: robot.GetRobotNorthState()}
+	r := &robot.Robot{State: robot.GetRobotNorthState(), MaxX: 5, MaxY: 5}
 	r.Move("MMMRMMLM")
 	fmt.Println(r.State.GetCordinate(), r.State.GetDirection())
 }
